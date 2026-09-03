b501af11c215362d4f93e8d8a3a23476908d63dd
Ahmed Hdhili <ahdhili832@gmail.com>
2026-09-03 19:15:48 +0100

    feat(admin): add client model

---
+++ admin_app/lib/models/client.dart
@@ -0,0 +1,8 @@
+class Client {
+  final String name;
+  final String email;
+  final String phone;
+  final int bookings;
+  final double revenue;
+  const Client({required this.name,required this.email,required this.phone,required this.bookings,required this.revenue});
+}
